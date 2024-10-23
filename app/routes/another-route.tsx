1f1adceb43c4afe9e4748c0b279e70a965941b15
Rajesh K <rajeshk@Rajeshs-MacBook-Pro.local>
2024-10-23 23:53:19 +0530

    testing

---
+++ app/routes/another-route.tsx
@@ -0,0 +1,11 @@
+export default function Dd() {
+  return (
+    <>
+      <div style={{ margin: "150px" }}>
+        <h1>
+          Another <strong>route</strong>
+        </h1>
+      </div>
+    </>
+  );
+}
